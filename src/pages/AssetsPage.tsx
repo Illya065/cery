faa328c1d9571254d12b9ffa716997806a819486
Illia Kalyna <illiakalyna.work@gmail.com>
2025-05-13 22:26:45 +0300

    add gitignore | fix build fail

--- src/pages/AssetsPage.tsx
+++ src/pages/AssetsPage.tsx
@@ -1,12 +1,18 @@
 import FiltersBlock from "@/components/FiltersBlock";
 import VehicleTable from "@/components/VehicleTable";
 import vehicles from "@/mock/mock_vehicles.json";
+import { Suspense } from "react";
 
 export default function AssetsPage() {
   return (
     <div className="flex flex-col gap-4 flex-1">
-      <FiltersBlock data={vehicles} />
-      <VehicleTable data={vehicles} />
+      <Suspense fallback={<div>Filters Loading...</div>}>
+        <FiltersBlock data={vehicles} />
+      </Suspense>
+
+      <Suspense fallback={<div>Vehicles Loading...</div>}>
+        <VehicleTable data={vehicles} />
+      </Suspense>
     </div>
   );
 }
